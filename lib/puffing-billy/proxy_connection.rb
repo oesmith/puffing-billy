de916e4cb892c423279937a33de6a0dc8547528c
Olly Smith <olly.smith@wildfireapp.com>
2012-10-05 19:16:07 +0100

    Split out request proxying.

--- lib/puffing-billy/proxy_connection.rb
+++ lib/puffing-billy/proxy_connection.rb
@@ -43,38 +43,17 @@ module Billy
       if @parser.http_method == 'CONNECT'
         restart_with_ssl(@parser.request_url)
       else
-        headers = Hash[@headers.map { |k,v| [k.downcase, v] }].merge('connection' => 'close')
         if @ssl
           url = "https://#{@ssl}#{@parser.request_url}"
         else
           url = @parser.request_url
         end
-        p @parser.http_method, url
-        req = EventMachine::HttpRequest.new(url)
-        req_opts = {
-          :redirects => 0,
-          :keepalive => false,
-          :head => headers,
-        }
-        req_opts[:body] = @body if @body
-        req = req.send(@parser.http_method.downcase, req_opts)
-
-        req.errback do
-          puts "Request failed: #{url}"
-          close_connection
-        end
-
-        req.callback do
-          res = EM::DelegatedHttpResponse.new(self)
-          res.status = req.response_header.status
-          res.headers = req.response_header
-          res.content = req.response
-          res.send_response
-        end
+        puts "#{@parser.http_method} #{url}"
+        handle_request(url)
       end
     end
 
-    private
+    protected
 
     def restart_with_ssl(url)
       @ssl = url
@@ -85,5 +64,35 @@ module Billy
         :cert_chain_file => File.expand_path('../mitm.crt', __FILE__)
       )
     end
+
+    def handle_request(url)
+      proxy_request(url)
+    end
+
+    def proxy_request(url)
+      headers = Hash[@headers.map { |k,v| [k.downcase, v] }].merge('connection' => 'close')
+      req = EventMachine::HttpRequest.new(url)
+      req_opts = {
+        :redirects => 0,
+        :keepalive => false,
+        :head => headers,
+      }
+      req_opts[:body] = @body if @body
+      req = req.send(@parser.http_method.downcase, req_opts)
+
+      req.errback do
+        puts "Request failed: #{url}"
+        close_connection
+      end
+
+      req.callback do
+        res = EM::DelegatedHttpResponse.new(self)
+        res.status = req.response_header.status
+        res.headers = req.response_header
+        res.content = req.response
+        res.send_response
+      end
+    end
+
   end
 end
