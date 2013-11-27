8fc508852b108e54a61c025e469727423a100439
tmertens <tim.mertens@centro.net>
2013-11-27 09:17:56 -0600

    Recursively sort nested arrays and hashes in json data structures.

--- lib/billy/resource_utils.rb
+++ lib/billy/resource_utils.rb
@@ -21,17 +21,37 @@ module Billy
       false
     end
 
-    def sort_hash(hash)
-      Hash[
-          hash.each do |k,v|
-            hash[k] = sort_hash(v) if v.class == Hash
-            hash[k] = v.collect {|a| sort_hash(a)} if v.class == Array
-          end.sort()
-      ]
+    # This method sorts JSON data, recursively and consistently to enable
+    #   consistent hashing of the JSON data string. It will recursively sort
+    #   data contained in nested JSON Hash and Array data structures.
+    def self.sort_json_data(data)
+      return data if !data.is_a? Enumerable
+
+      p = proc { |d|
+        d.collect do |item|
+          d.is_a?(Hash) ? [item[0],sort_json_data(item[1])] : sort_json_data(item)
+        end.group_by do |i|
+          # Group by class to avoid comparison errors
+          i.class
+        end
+      }
+
+      result = []
+      p.call(data).each do |group|
+        # Actually sort the data
+        result.concat(group[1].sort do |v1,v2|
+          # Enumerables sometimes cannot be compared using the default <=> operator,
+          # but we can compare the hash of the objects, as it returns consistent
+          # results: ({b:"a",a:"b"}.hash == {a:"b",b:"a"}.hash) => true
+          (v1.is_a? Enumerable) ? v1.hash <=> v2.hash : v1 <=> v2
+        end)
+      end
+
+      data.is_a?(Hash) ? Hash[result] : result
     end
 
     def sort_json(json_str)
-      sort_hash(JSON.parse(json_str, symbolize_names: true)).to_json
+      ResourceUtils.sort_json_data(JSON.parse(json_str, symbolize_names: true)).to_json
     end
   end
 end
