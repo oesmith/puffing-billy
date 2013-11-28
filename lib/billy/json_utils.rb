baa1666c6573621d4ff911d94e126015696b25d2
Frank O'Hara <frankj.ohara@gmail.com>
2013-11-28 07:52:14 -0600

    Sort name/value pairs of JSON objects, but preserve array order.

--- lib/billy/json_utils.rb
+++ lib/billy/json_utils.rb
@@ -10,32 +10,31 @@ module Billy
       false
     end
 
-    # This method sorts JSON data, recursively and consistently to enable
-    #   consistent hashing of the JSON data string. It will recursively sort
-    #   data contained in nested JSON Hash and Array data structures.
-    def self.sort_json_data(data)
-      return data unless data.is_a? Enumerable
-
-      result = if data.is_a?(Hash)
-          data.collect { |k,v| [k,sort_json_data(v)] }
-        else
-          data.collect { |item| sort_json_data(item) }
-        end.group_by(&:class).values.map do |values|
-          # Actually sort the data
-          values.sort do |v1,v2|
-            r = v1 <=> v2
-
-            # Sometimes objects cannot be compared using the default <=> operator,
-            #   so we use the string value of the object for comparison:
-            [-1,0,1].include?(r) ? r : v1.to_s <=> v2.to_s
-          end
-        end.inject(:+)
-
-      data.is_a?(Hash) ? Hash[result] : result
+    # Recursively sorts the key/value pairs of all hashes within the given
+    #   data structure while preserving the order of arrays.
+    def sort_hash_keys(data)
+      return data unless data.is_a?(Hash) || data.is_a?(Array)
+      if data.is_a? Hash
+        data.keys.sort.reduce({}) do |seed, key|
+          seed[key] = sort_hash_keys(data[key])
+          seed
+        end
+      else
+        data.map do |element|
+          sort_hash_keys(element)
+        end
+      end
     end
 
+    # Recursively sorts the name/value pairs of JSON objects. For instance,
+    #   sort_json('{"b" : "2", "a" : "1"}') == sort_json('{"a" : "1", "b" : "2"}')
+    #   Per http://json.org/, "An object is an unordered set of name/value pairs"
+    #   and "An array is an ordered collection of values". So, we can sort the
+    #   name/value pairs by name (key), but we must preserve the order of an array.
+    #   Processing JSON in this way enables a consistent SHA to be derived from
+    #   JSON payloads which have the same name/value pairs, but different orders.
     def sort_json(json_str)
-      JSONUtils.sort_json_data(JSON.parse(json_str, symbolize_names: true)).to_json
+      sort_hash_keys(JSON.parse(json_str, symbolize_names: true)).to_json
     end
   end
 end
--- spec/lib/billy/resource_utils_spec.rb
+++ spec/lib/billy/resource_utils_spec.rb
@@ -3,35 +3,33 @@ include Billy::JSONUtils
 
 describe Billy::JSONUtils do
   describe 'sorting' do
-    describe '#sort_json_data' do
+    describe '#sort_hash_keys' do
       it 'sorts simple Hashes' do
         data     = {c: 'three',a: 'one',b: 'two'}
         expected = {a: 'one',b: 'two',c: 'three'}
-        expect(Billy::JSONUtils.sort_json_data(data)).to eq expected
+        expect(sort_hash_keys(data)).to eq expected
       end
 
-      it 'sorts simple Arrays' do
+      it 'does not sort simple Arrays' do
         data     = [3,1,2,'two','three','one']
-        expected = [1,2,3,'one','three','two']
-        expect(Billy::JSONUtils.sort_json_data(data)).to eq expected
+        expect(sort_hash_keys(data)).to eq data
       end
 
-      it 'sorts multi-dimensional Arrays' do
+      it 'does not sort multi-dimensional Arrays' do
         data     = [[3,2,1],[5,4,6],['b','c','a']]
-        expected = [['a','b','c'],[1,2,3],[4,5,6]]
-        expect(Billy::JSONUtils.sort_json_data(data)).to eq expected
+        expect(sort_hash_keys(data)).to eq data
       end
 
       it 'sorts multi-dimensional Hashes' do
         data     = {c: {l: 2,m: 3,k: 1},a: {f: 3,e: 2,d: 1},b: {i: 2,h: 1,j: 3}}
         expected = {a: {d: 1,e: 2,f: 3},b: {h: 1,i: 2,j: 3},c: {k: 1,l: 2,m: 3}}
-        expect(Billy::JSONUtils.sort_json_data(data)).to eq expected
+        expect(sort_hash_keys(data)).to eq expected
       end
 
       it 'sorts abnormal data structures' do
         data     = {b: [['b','c','a'],{ab: 5,aa: 4, ac: 6},[3,2,1],{ba: true,bc: false, bb: nil}],a: {f: 3,e: 2,d: 1}}
-        expected = {a: {d: 1,e: 2,f: 3},b: [['a','b','c'],[1,2,3],{aa: 4,ab: 5,ac: 6},{ba: true, bb: nil,bc: false}]}
-        expect(Billy::JSONUtils.sort_json_data(data)).to eq expected
+        expected = {a: {d: 1,e: 2,f: 3},b: [['b','c','a'],{aa: 4,ab: 5,ac: 6},[3,2,1],{ba: true, bb: nil,bc: false}]}
+        expect(sort_hash_keys(data)).to eq expected
       end
     end
 
