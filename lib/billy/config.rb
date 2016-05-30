a497a8f65c6f95c30d918a514b954972f04d2c42
Maarten Ackermans <maarten.ackermans@gmail.com>
2016-05-30 18:54:10 +0200

    Requests to stubs are now able to be retrieved

--- lib/billy/config.rb
+++ lib/billy/config.rb
@@ -10,7 +10,7 @@ module Billy
                   :persist_cache, :ignore_cache_port, :non_successful_cache_disabled, :non_successful_error_level,
                   :non_whitelisted_requests_disabled, :cache_path, :proxy_host, :proxy_port, :proxied_request_inactivity_timeout,
                   :proxied_request_connect_timeout, :dynamic_jsonp, :dynamic_jsonp_keys, :merge_cached_responses_whitelist,
-                  :strip_query_params, :proxied_request_host, :proxied_request_port, :cache_request_body_methods
+                  :strip_query_params, :proxied_request_host, :proxied_request_port, :cache_request_body_methods, :record_stub_requests
 
     def initialize
       @logger = defined?(Rails) ? Rails.logger : Logger.new(STDOUT)
@@ -40,6 +40,7 @@ module Billy
       @proxied_request_host = nil
       @proxied_request_port = 80
       @cache_request_body_methods = ['post']
+      @record_stub_requests = false
     end
   end
 
