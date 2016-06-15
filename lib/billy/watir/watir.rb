96b8c84c795b8660e2341f6996c995776c0e8043
Maarten Ackermans <maarten.ackermans@gmail.com>
2016-06-15 16:07:18 +0200

    Added support for Watir :phantomjs driver

--- lib/billy/watir/watir.rb
+++ lib/billy/watir/watir.rb
@@ -4,22 +4,29 @@ module Billy
   def self.browser(name, args = {})
     if(name === :chrome)
       switches = args[:switches]
-
       if switches.nil?
         switches = @settings[:chrome][:switches]
       else
         switches.push(*@settings[:chrome][:switches])
       end
+      args[:switches] = switches
 
       @browser = Watir::Browser.new :chrome, **args
+    elsif(name === :phantomjs)
+      additional_args = args[:args]
+      if additional_args.nil?
+        additional_args = @settings[:phantomjs][:args]
+      else
+        additional_args.push(*@settings[:phantomjs][:args])
+      end
+      args[:args] = additional_args
+      @browser = Watir::Browser.new :phantomjs, **args
     else
       profile = args[:profile]
-
       if profile.nil?
         profile = @settings[:firefox][:profile]
       end
       profile.proxy = @settings[:firefox][:proxy]
-
       args[:profile] = profile
 
       @browser = Watir::Browser.new :firefox, **args
@@ -38,16 +45,20 @@ module Billy
     @settings = {}
 
     if defined?(Selenium::WebDriver)
+      @settings[:chrome] = {
+        switches: %W[--proxy-server=#{Billy.proxy.host}:#{Billy.proxy.port}]
+      }
+
+      @settings[:phantomjs] = {
+        args: %W[--proxy=#{Billy.proxy.host}:#{Billy.proxy.port}]
+      }
+
       @settings[:firefox] = {
         profile: Selenium::WebDriver::Firefox::Profile.new,
         proxy: Selenium::WebDriver::Proxy.new(
           http: "#{Billy.proxy.host}:#{Billy.proxy.port}",
           ssl: "#{Billy.proxy.host}:#{Billy.proxy.port}"
         )
-      }
-
-      @settings[:chrome] = {
-        switches: %W[--proxy-server=#{Billy.proxy.host}:#{Billy.proxy.port}]
       }
     end
   end
