c6346c534b118523713e16e3ac2cd7bb6e1665a7
Maarten Ackermans <maarten.ackermans@gmail.com>
2016-06-27 17:14:44 +0200

    Refactored capybara class

--- lib/billy/browsers/capybara.rb
+++ lib/billy/browsers/capybara.rb
@@ -4,52 +4,58 @@ module Billy
   module Browsers
     class Capybara
 
+      DRIVERS = {
+        poltergeist: 'capybara/poltergeist',
+        webkit: 'capybara/webkit',
+        selenium: 'selenium/webdriver'
+      }
+
       def self.register_drivers
-        ['capybara/poltergeist', 'capybara/webkit', 'selenium/webdriver'].each do |driver|
-          begin
-            require driver
-          rescue LoadError
-          end
+        DRIVERS.each do |name, driver|
+          require driver rescue next
+          send("register_#{name}_driver")
+        end
+      end
+
+      private
+
+      def self.register_poltergeist_driver
+        ::Capybara.register_driver :poltergeist_billy do |app|
+          options = {
+            phantomjs_options: [
+              '--ignore-ssl-errors=yes',
+              "--proxy=#{Billy.proxy.host}:#{Billy.proxy.port}"
+            ]
+          }
+          ::Capybara::Poltergeist::Driver.new(app, options)
+        end
+      end
+
+      def self.register_webkit_driver
+        ::Capybara.register_driver :webkit_billy do |app|
+          options = {
+            ignore_ssl_errors: true,
+            proxy: {host: Billy.proxy.host, port: Billy.proxy.port}
+          }
+          ::Capybara::Webkit::Driver.new(app, ::Capybara::Webkit::Configuration.to_hash.merge(options))
+        end
+      end
+
+      def self.register_selenium_driver
+        ::Capybara.register_driver :selenium_billy do |app|
+          profile = Selenium::WebDriver::Firefox::Profile.new
+          profile.assume_untrusted_certificate_issuer = false
+          profile.proxy = Selenium::WebDriver::Proxy.new(
+            http: "#{Billy.proxy.host}:#{Billy.proxy.port}",
+            ssl: "#{Billy.proxy.host}:#{Billy.proxy.port}")
+          ::Capybara::Selenium::Driver.new(app, profile: profile)
         end
 
-        if defined?(::Capybara::Poltergeist)
-          ::Capybara.register_driver :poltergeist_billy do |app|
-            options = {
-              phantomjs_options: [
-                '--ignore-ssl-errors=yes',
-                "--proxy=#{Billy.proxy.host}:#{Billy.proxy.port}"
-              ]
-            }
-            ::Capybara::Poltergeist::Driver.new(app, options)
-          end
-        end
-
-        if defined?(::Capybara::Webkit::Driver)
-          ::Capybara.register_driver :webkit_billy do |app|
-            options = {
-              ignore_ssl_errors: true,
-              proxy: {host: Billy.proxy.host, port: Billy.proxy.port}
-            }
-            ::Capybara::Webkit::Driver.new(app, ::Capybara::Webkit::Configuration.to_hash.merge(options))
-          end
-        end
-
-        if defined?(Selenium::WebDriver)
-          ::Capybara.register_driver :selenium_billy do |app|
-            profile = Selenium::WebDriver::Firefox::Profile.new
-            profile.assume_untrusted_certificate_issuer = false
-            profile.proxy = Selenium::WebDriver::Proxy.new(
-              http: "#{Billy.proxy.host}:#{Billy.proxy.port}",
-              ssl: "#{Billy.proxy.host}:#{Billy.proxy.port}")
-            ::Capybara::Selenium::Driver.new(app, profile: profile)
-          end
-
-          ::Capybara.register_driver :selenium_chrome_billy do |app|
-            ::Capybara::Selenium::Driver.new(
-              app, browser: :chrome,
-              switches: ["--proxy-server=#{Billy.proxy.host}:#{Billy.proxy.port}"]
-            )
-          end
+        ::Capybara.register_driver :selenium_chrome_billy do |app|
+          ::Capybara::Selenium::Driver.new(
+            app, browser: :chrome,
+            switches: ["--proxy-server=#{Billy.proxy.host}:#{Billy.proxy.port}"]
+          )
         end
       end
 
