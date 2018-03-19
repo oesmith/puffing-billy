29d935829ff1161007261ca71e4d4957932c9b2e
Alex Kube <alex@botlink.com>
2018-03-20 02:29:17 +0430

    Optionally pass options/profiles when registering drivers

--- lib/billy/browsers/capybara.rb
+++ lib/billy/browsers/capybara.rb
@@ -3,7 +3,6 @@ require 'billy'
 module Billy
   module Browsers
     class Capybara
-
       DRIVERS = {
         poltergeist: 'capybara/poltergeist',
         webkit: 'capybara/webkit',
@@ -20,49 +19,60 @@ module Billy
         end
       end
 
-      def self.register_poltergeist_driver
+      def self.register_poltergeist_driver(options = {})
         ::Capybara.register_driver :poltergeist_billy do |app|
-          options = {
+          options.merge(
             phantomjs_options: [
               '--ignore-ssl-errors=yes',
               "--proxy=#{Billy.proxy.host}:#{Billy.proxy.port}"
             ]
-          }
+          )
           ::Capybara::Poltergeist::Driver.new(app, options)
         end
       end
 
-      def self.register_webkit_driver
+      def self.register_webkit_driver(options = {})
         ::Capybara.register_driver :webkit_billy do |app|
-          options = {
+          options.merge(
             ignore_ssl_errors: true,
-            proxy: {host: Billy.proxy.host, port: Billy.proxy.port}
-          }
-          ::Capybara::Webkit::Driver.new(app, ::Capybara::Webkit::Configuration.to_hash.merge(options))
-        end
-      end
-
-      def self.register_selenium_driver
-        ::Capybara.register_driver :selenium_billy do |app|
-          profile = Selenium::WebDriver::Firefox::Profile.new
-          profile.assume_untrusted_certificate_issuer = false
-          profile.proxy = Selenium::WebDriver::Proxy.new(
-            http: "#{Billy.proxy.host}:#{Billy.proxy.port}",
-            ssl: "#{Billy.proxy.host}:#{Billy.proxy.port}")
-          ::Capybara::Selenium::Driver.new(app, profile: profile)
-        end
-
-        ::Capybara.register_driver :selenium_chrome_billy do |app|
-          options = Selenium::WebDriver::Chrome::Options.new
-          options.add_argument("--proxy-server=#{Billy.proxy.host}:#{Billy.proxy.port}")
-
-          ::Capybara::Selenium::Driver.new(
-            app, browser: :chrome,
-            options: options
+            proxy: { host: Billy.proxy.host, port: Billy.proxy.port }
+          )
+          ::Capybara::Webkit::Driver.new(
+            app,
+            ::Capybara::Webkit::Configuration.to_hash.merge(options)
           )
         end
       end
 
+      def self.register_selenium_driver
+        register_selenium_firefox
+        register_selenium_chrome
+      end
+
+      def self.register_selenium_firefox(profile)
+        ::Capybara.register_driver :selenium_billy do |app|
+          profile ||= Selenium::WebDriver::Firefox::Profile.new
+          profile.assume_untrusted_certificate_issuer = false
+          profile.proxy = Selenium::WebDriver::Proxy.new(
+            http: "#{Billy.proxy.host}:#{Billy.proxy.port}",
+            ssl: "#{Billy.proxy.host}:#{Billy.proxy.port}"
+          )
+          ::Capybara::Selenium::Driver.new(app, profile: profile)
+        end
+      end
+
+      def self.register_selenium_chrome(options)
+        ::Capybara.register_driver :selenium_chrome_billy do |app|
+          options ||= Selenium::WebDriver::Chrome::Options.new
+          options.add_argument(
+            "--proxy-server=#{Billy.proxy.host}:#{Billy.proxy.port}"
+          )
+
+          ::Capybara::Selenium::Driver.new(
+            app, browser: :chrome, options: options
+          )
+        end
+      end
     end
   end
 end
