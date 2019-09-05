e416e9fb2d5f2dbaa7c2a9ee814889be05b1c78d
Andrew Walter <andrew@xsm.fyi>
2019-09-05 15:12:38 +1000

    Remove deprecation warning in Capybara.register_selenium_driver
    Fixes "Selenium [DEPRECATION] :profile is deprecated. Use Selenium::WebDriver::Firefox::Options#profile= instead." warning

--- lib/billy/browsers/capybara.rb
+++ lib/billy/browsers/capybara.rb
@@ -47,12 +47,15 @@ module Billy
 
       def self.register_selenium_driver
         ::Capybara.register_driver :selenium_billy do |app|
-          profile = Selenium::WebDriver::Firefox::Profile.new
-          profile.assume_untrusted_certificate_issuer = false
-          profile.proxy = Selenium::WebDriver::Proxy.new(
-            http: "#{Billy.proxy.host}:#{Billy.proxy.port}",
-            ssl: "#{Billy.proxy.host}:#{Billy.proxy.port}")
-          ::Capybara::Selenium::Driver.new(app, profile: profile)
+          profile = Selenium::WebDriver::Firefox::Profile.new.tap do |prof|
+            prof.assume_untrusted_certificate_issuer = false
+            prof.proxy = Selenium::WebDriver::Proxy.new(
+              http: "#{Billy.proxy.host}:#{Billy.proxy.port}",
+              ssl: "#{Billy.proxy.host}:#{Billy.proxy.port}")
+          end
+          options = Selenium::WebDriver::Firefox::Options.new(profile: profile)
+          
+          ::Capybara::Selenium::Driver.new(app, options: options)
         end
 
         ::Capybara.register_driver :selenium_chrome_billy do |app|
