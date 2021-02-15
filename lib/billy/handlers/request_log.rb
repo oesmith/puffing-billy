52479f5f0d78c1313ccbca025aecf5d74df2c0d4
snackattas <zach.attas@gmail.com>
2021-02-14 22:24:56 -0600

    poc of idea

--- lib/billy/handlers/request_log.rb
+++ lib/billy/handlers/request_log.rb
@@ -1,9 +1,23 @@
 module Billy
   class RequestLog
-    attr_reader :requests
+    attr_reader :requests, :har
 
     def initialize
       @requests = []
+      if Billy.config.record_proxy_as_har
+        @requests = {
+          log: {
+              version: '1.1',
+              creator: { name: 'Puffing Billy' },
+              pages: [{
+                startedDateTime: Time.now.strftime('%FT%T.%3N%z'),
+                id: 'proxy.har',
+                title: 'proxy.har',
+                pageTimings: { onContentLoad: -1, onLoad: -1 }
+              }],
+              entries: []
+          }
+        }
     end
 
     def reset
@@ -13,15 +27,21 @@ module Billy
     def record(method, url, headers, body)
       return unless Billy.config.record_requests
 
-      request = {
-        status: :inflight,
-        handler: nil,
-        method: method,
-        url: url,
-        headers: headers,
-        body: body
-      }
-      @requests.push(request)
+      if Billy.config.record_proxy_as_har
+        entry = {abc: 5}
+        require 'pry'; binding.pry
+        @requests[:log][:entries].push(entry)
+      else
+        request = {
+          status: :inflight,
+          handler: nil,
+          method: method,
+          url: url,
+          headers: headers,
+          body: body
+        }
+        @requests.push(request)
+      end
 
       request
     end
